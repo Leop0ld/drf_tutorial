dc7b7ba5b57aac24f82ad2472efb89ce76a958ea
Leop0ld <l3opold7@gmail.com>
2017-09-11 21:23:33 +0900

    :zap: Users API 추가

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
+
 from snippets import views
 
 urlpatterns = [
     url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)$', views.SnippetDetail.as_view()),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
 
 # format_suffix_patterns : 이거 어떤거 하는건지? 설명 제대로 안되어있는듯
